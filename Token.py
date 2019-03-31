9e86172c6e59ed029915a898e7a7ad4751cecd0f
xrsheeran <xqu25@wisc.edu>
2019-03-31 10:41:56 -0500

    added

--- Token.py
+++ Token.py
@@ -1,2 +1,14 @@
-//for filename in glob.glob(os.path.join(path, '*.txt')):
-//
+import collections
+import glob
+import os
+
+path='./news/'
+c=collections.Counter()
+for fn in glob.glob(os.path.join(path,'*.txt')):
+    #got names of txt
+    count=0;
+    with open(fn) as f:
+        for ln in f:
+            for word in ln.split():
+                c[word]+=1
+print(c.most_common(3))
